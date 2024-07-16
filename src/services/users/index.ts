975378737d452b7e188e2da80bc10503c2b6f0fe
rayen <rayendkl1234@gmail.com>
2024-07-16 16:52:03 +0100

    fix: fix  table in  index file

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,19 +1,19 @@
 import { executeSQLQuery } from "../../database";
 
 export const getUser = async (login: string, password: string) => {
-    const query = `SELECT * FROM public.users WHERE login='${login}' and password='${password}'`;
+    const query = `SELECT * FROM public."userTable" WHERE login='${login}' and password='${password}'`;
     const result = await executeSQLQuery(query);
     return result.rows;
 };
 
-export const addUser = async (login: string, password: string) => {
-    const query = `INSERT INTO public.users(login,password) VALUES ('${login}','${password}')`;
+export const addUser = async (login: string, password: string, phone : number,gender  : string,name  : string) => {
+    const query = `INSERT INTO public."userTable"(login,password ,phone,gender,name) VALUES ('${login}','${password}','${phone}','${gender}','${name}')`;
     const result = await executeSQLQuery(query);
     return result.rows[0];
 };
 
 export const deleteUser = async (login: string) => {
-    const query = `DELETE  FROM public.users WHERE login='${login}' returning login`;
+    const query = `DELETE  FROM public.userDB WHERE login='${login}' returning login`;
     const result = await executeSQLQuery(query);
     return result.rows[0];
 };
